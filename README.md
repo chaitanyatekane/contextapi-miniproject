dd1ce82b0d1d0c0c0cc55c75fcc06df84f37d669
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2022-02-17 09:15:30 +0530

    add faker

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### useContext - Mini-Project :-
 
--> npm i react-router-dom
+-> npm i react-router-dom@5
+-> npm i faker@5.5.3
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,16 @@
 import React from "react";
+import faker from "faker";
 
 const Home = () => {
+  const productsArray = [...Array(20)].map(() => ({
+    id: faker.datatype.uuid(),
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    image: faker.random.image(),
+  }));
+
+  console.log(productsArray);
+
   return <div>Home</div>;
 };
 
