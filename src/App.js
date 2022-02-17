24058a42e5d9b5d73ce2eee6e394cd2796b227ab
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2022-02-17 08:53:24 +0530

    basic setup of react-router v5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Header />
-    </div>
+      <div className="App">
+        {/* <Switch> */}
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        {/* </Switch> */}
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <span>ReactContext API Mini-Project</span>
+      <ul className="nav">
+        <li>
+          <Link to="/">Homepage</Link>
+        </li>
+        <li>
+          <Link to="/cart">Cart</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Header;
