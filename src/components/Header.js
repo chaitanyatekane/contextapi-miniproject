1437711c37f5eecd3dd0914ca120b68bccd3b426
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2022-02-17 09:01:51 +0530

    add styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./styles.css";
 
 const Header = () => {
   return (
     <div>
-      <span>ReactContext API Mini-Project</span>
+      <span className="header">ReactContext API Mini-Project</span>
       <ul className="nav">
         <li>
           <Link to="/">Homepage</Link>
